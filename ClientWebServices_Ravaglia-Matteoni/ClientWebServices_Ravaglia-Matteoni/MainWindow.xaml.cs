d85e4c97fdad51c899d055e7111756463073fe8e
Giacomo Ravaglia <giacomo.ravaglia@local.ittcesena.it>
2019-02-07 08:12:12 +0100

    First commit: creation of the client

---
+++ ClientWebServices_Ravaglia-Matteoni/ClientWebServices_Ravaglia-Matteoni/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Http;
+
+namespace ClientWebServices_Ravaglia_Matteoni
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        async static void GetRequest(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                // Richiesta al server
+                using (HttpResponseMessage response = await (client.GetAsync(url)))
+                {
+                    // Estrazione del contenuto
+                    using (HttpContent content = response.Content)
+                    {
+                        string myContent = await (content.ReadAsStringAsync());
+
+                        MessageBox.Show(myContent);
+                    }
+                }
+            }
+        }
+
+        private void btn_Get_Click(object sender, RoutedEventArgs e)
+        {
+            // URL a cui inoltrare la richiesta
+            string url = @"http://10.13.100.35/Rava/WebServices/" +
+                         "?id=1";                      
+
+            GetRequest(url);
+        }
+    }
+}
