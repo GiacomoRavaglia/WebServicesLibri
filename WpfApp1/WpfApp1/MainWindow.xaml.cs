4c33381b5bbfd828c0c2232837e2a80ec2bd3992
leonardomatteoni1999 <leonardo.matteoni125@gmail.com>
2019-02-14 09:47:29 +0100

    client p4

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -20,9 +20,57 @@ namespace Client1
     /// </summary>
     public partial class MainWindow : Window
     {
+        static string mycontent;
+        static ListBox lst;
         public MainWindow()
         {
             InitializeComponent();
+            lst = 1;
+        }
+
+
+
+        private void btn_richiesta1_Click(object sender, RoutedEventArgs e)
+        {
+            string url = "http://10.13.100.2/Lolli/Tp/Server.php?op=1&rep=" + 1.Text.ToLower() + "&sez=" + 2.Text.ToLower();
+            Richiesta(url);
+        }
+
+        private void btn_richiesta2_Click(object sender, RoutedEventArgs e)
+        {
+            string url = "http://10.13.100.2/Lolli/Tp/Server.php?op=2";
+            Richiesta(url);
+        }
+
+        private void btn_richiesta3_Click_1(object sender, RoutedEventArgs e)
+        {
+            string url = "http://10.13.100.2/Lolli/Tp/Server.php?op=3&d1=" + 1.Text + "&d2=" + 2.Text;
+            Richiesta(url);
+        }
+
+        private void btn_richiesta_4_Click(object sender, RoutedEventArgs e)
+        {
+            string url = "http://10.13.100.2/Lolli/Tp/Server.php?op=4&id=" + 1.Text;
+            Richiesta(url);
+        }
+
+        async static void Richiesta(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage response = await client.GetAsync(url))
+                {
+                    using (HttpContent content = response.Content)
+                    {
+                        mycontent = await content.ReadAsStringAsync();
+                        string.spl
+                        lst.Items.Clear();
+                        lst.Items.Add(mycontent);
+                    }
+
+                }
+
+            }
         }
     }
 }
